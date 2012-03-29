4533d1be18fd19a424c85383006948fc511240a8
Mukul Panchal <mukulpanchal@gmail.com>
2012-03-29 02:10:04 -0700

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Mukul Panchal"
+    email    "mukulpanchal@gmail.com"
+    password "hello1234"
+    password_confirmation "hello1234"
+  end
+end
